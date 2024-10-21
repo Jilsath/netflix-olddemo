7c94c15b602cb56c558ec2c58a79ecd0b6e3527f
Jilsa <jilsalijas22@gmail.com>
2024-10-21 17:51:26 +0400

    first commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <h3 className='copyright'>copyright ©2024 Jilsa - All Rights Reserved</h3>
+      
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseurl='https://api.themoviedb.org/3';
+export const API_KEY="384e49a8c5bd754fbeb4bdc5d8c0255c";
+export const imageUrl='https://image.tmdb.org/t/p/original';
